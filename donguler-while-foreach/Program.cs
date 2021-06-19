dc666fc3b2277a5193128e46f7121f8ca69a95e9
Yasin Gulcu <ysg020813@gmail.com>
2021-06-20 02:26:56 +0300

    9.Ödev Eklendi

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While
+            // 1'den başlayarak console'dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console'a yazdıran program.
+            // Console.WriteLine("Lütfen bir sayı girin: );
+            // int sayi = int.Parse(Console.ReadLine());
+            // int sayac = 1;
+            // int toplam = 0;
+            // while (sayac <= sayi)
+            // {
+            //      toplam += sayac;
+            //      sayac++;   
+            // }
+            // Console.WriteLine(toplam / sayi);
+
+            // 'a' - 'z' tüm harfleri console'a yazdırın.
+            // char character = 'a';
+            // while (character = < 'z')
+            // {
+            //      Console.Write(character);
+            //      character++;    
+            // }
+
+            Console.Write("***** Foreach *****");
+            String[] arabalar = {"BMV", "Ford", "Toyota", "Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
