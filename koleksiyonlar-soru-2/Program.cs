31f2adf76dc05e55b5a75ad3797c071561070bb0
Yasin Gulcu <ysg020813@gmail.com>
2021-06-20 23:46:58 +0300

    Koleksiyon Soruları Yapıldı

---
+++ koleksiyonlar-soru-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace koleksiyonlar_soru_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("20 afet pozitif sayi giriniz");
+            
+            ArrayList sayi = new ArrayList();
+            
+
+
+            for(int i = 0; i<20; i++){
+                System.Console.WriteLine("{0}. sayıyı giriniz", i+1);
+
+                int x = int.Parse(Console.ReadLine());
+                sayi.Add(i);
+            }
+
+            sayi.Sort();
+            int max3 = 0;
+            for(int i = sayi.Count; i >= sayi.Count-3; i++){
+                max3 += i;
+            }
+            System.Console.WriteLine("max 3 : " + max3);
+
+            int min3 = 0;
+            for(int i = 0; i >= 3; i++){
+                min3 += i;
+            }
+            System.Console.WriteLine("min 3 : " + max3);
+
+
+            Console.ReadLine();
+        }
+    }
+}
