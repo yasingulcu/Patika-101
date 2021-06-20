c02709a47b15e60f184db69fe3d469dc66380c57
Yasin Gulcu <ysg020813@gmail.com>
2021-06-20 23:51:56 +0300

    Koleksiyon Soruları Yapıldı

---
+++ koleksiyonlar-soru-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace koleksiyonlar_soru_3
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir cümle giriniz: ");
+            string sentence = Console.ReadLine();
+            string vowel = "aeıioüuü";
+
+            List<char> vowelsFromSentence = new List<char>();
+
+            foreach (char letter in sentence)
+            {
+                if (vowel.Contains(letter))
+                {
+                    vowelsFromSentence.Add(letter);
+                }
+            }
+
+            vowelsFromSentence.Sort();
+
+            foreach (char letter in vowelsFromSentence)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
