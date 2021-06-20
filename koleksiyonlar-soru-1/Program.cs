83149b5f5714bb5ec8c55902337a8af7eac27d0b
Yasin Gulcu <ysg020813@gmail.com>
2021-06-20 23:43:50 +0300

    Koleksiyon Soruları Yapıldı

---
+++ koleksiyonlar-soru-1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+
+namespace koleksiyonlar_soru_1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("20 afet pozitif sayi giriniz");
+            
+            ArrayList asal = new ArrayList();
+            ArrayList nonasal = new ArrayList();
+            int aToplam=0;
+            int nToplam =0;
+
+
+            for(int i = 0; i<20; i++){
+                System.Console.WriteLine("{0}. sayıyı giriniz", i+1);
+
+                string x = Console.ReadLine();
+
+                if(int.TryParse(x, out int number) && number >= 0)
+                {
+                    if(Prime(number)){
+                        asal.Add(number);
+                    }else {
+                        nonasal.Add(number);
+                    }
+                }
+            }
+
+            asal.Sort();
+            nonasal.Sort();
+
+            System.Console.WriteLine("*** Sıralı aritmetik sayılar ***");
+            foreach (var item in asal)
+            {
+                System.Console.WriteLine(item);
+            }
+
+
+            System.Console.WriteLine("*** Sıralı aritmetik olmayan sayılar ***");
+            foreach (var item in nonasal)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            for(int i = 0; i < asal.Count; i++){
+                aToplam += i;
+            }
+
+            for(int i = 0; i < nonasal.Count; i++){
+                nToplam += i;
+            }
+
+            System.Console.WriteLine("Asal sayıların ortalaması {0}",aToplam/asal.Count);
+            System.Console.WriteLine("Asal olmayan sayıların ortalaması {0}",nToplam/nonasal.Count);
+
+            Console.ReadLine();
+
+        }
+
+
+        public static bool Prime(int n){
+        int i, m=0;
+        m = n/2;   
+        for(i = 2; i <= m; i++)    
+          {    
+           if(n % i == 0)    
+            {    
+             return true;  
+            }  
+          }    
+          return false;        
+        }
+    }
+}
